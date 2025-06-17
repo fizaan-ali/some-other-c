f799a7628816e1bd7d83fe33fe6a16a01972a32d
𝑭𝑰𝒁𝑨𝑨𝑵 <fizaan.mughal.01@gmail.com>
2025-06-17 13:15:10 +0500

    Auto update

---
+++ strpointer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str1[30], str2[30];
+    printf("Enter the string: ");
+    gets(str1);
+    char *end1; char *start2;
+    end1 = str1 + strlen(str1)-1;
+    start2 = str2;
+    while (end1>=str1)
+    {
+        *start2 = *end1;
+        end1--;
+        start2++;
+    }
+    *start2 = '\0';
+
+    printf("Original String: %s\n", str1);
+    printf("Reverse string: %s", str2);
+    
+    return 0;
+}
